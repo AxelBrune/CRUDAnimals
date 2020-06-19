7ae9e41757e11378d589e84b8bdd4f7f78357076
Axel Brune <axelbrune50@gmail.com>
2020-06-19 18:52:26 +0200

    Ajout de router-vue

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,14 +4,16 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>AnimalCRUD</title>
+        <title>Mon premier composant</title>
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     </head>
-    <body>
-        <div class="container" id="app">
-            <example-component></example-component>
+    <body>        
+        <div id="app">
+            <h1>Application CRUD laravel+VueJS</h1>
+            <div class="container">
+                <router-view></router-view>
+            </div>
         </div>
-
         <script src="{{ asset('js/app.js') }}"></script>
     </body>
 </html>
